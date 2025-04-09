8facad1c80287c5dd76b6c9199bb5244bfc97509
usernamenul1 <usernamenul1@outlook.com>
2025-04-09 19:27:09 +0800

    initial

---
+++ rustgit/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod rust_git {
+    pub fn git_init() {}
+
+    pub fn git_add(args: Vec<&String>) {}
+
+    pub fn git_commit(args: Vec<&String>) {}
+
+    pub fn git_branch(args: Vec<&String>) {}
+
+    pub fn git_checkout(args: Vec<&String>) {}
+
+    pub fn git_merge(args: Vec<&String>) {}
+
+    pub fn git_fetch(args: Vec<&String>) {}
+
+    pub fn git_pull(args: Vec<&String>) {}
+
+    pub fn git_push(args: Vec<&String>) {}
+
+    pub fn git_status() {}
+}
